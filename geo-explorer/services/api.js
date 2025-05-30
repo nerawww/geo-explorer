e151d304aa8c104037c5d05f88b3d120198446a2
FischerTheo <fischer.theo2412@gmail.com>
2025-05-30 09:21:42 +0200

    Delete geo-explorer directory

--- geo-explorer/services/api.js
+++
@@ -1,7 +0,0 @@
-export async function getCountryByName(name) {
-  const response = await fetch(`https://restcountries.com/v3.1/name/${encodeURIComponent(name)}`);
-  if (!response.ok) {
-    throw new Error('Erreur lors de la récupération du pays');
-  }
-  return response.json();
-}
